09f328649bc32bb7d26ec4c74e5e688b6a8aa17c
Loujin Alsalama <loujinsa48@gmail.com>
2025-06-04 22:22:48 +0300

    Add edit task for mvc project

--- MvcTodoApp_SV/obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
+++ MvcTodoApp_SV/obj/Debug/net9.0/MvcTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f92194d22fe6d1023b764f6bb6aad7abaeb1c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
